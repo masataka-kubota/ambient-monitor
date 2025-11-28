f6aef6d6c7f8b835403b4a22ae397d0ce0415b47
packerman1212 <mezase226334@gmail.com>
2025-11-28 14:18:24 +0900

    feat(mobile): persist themeModeAtom using AsyncStorage

--- packages/mobile/atoms/themeAtom.ts
+++ packages/mobile/atoms/themeAtom.ts
@@ -1,5 +1,13 @@
-import { atomWithStorage } from "jotai/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { atomWithStorage, createJSONStorage } from "jotai/utils";
 
 import { ThemeMode } from "@/types";
 
-export const themeModeAtom = atomWithStorage<ThemeMode>("themeMode", "system");
+const storage = createJSONStorage<ThemeMode>(() => AsyncStorage);
+
+export const themeModeAtom = atomWithStorage<ThemeMode>(
+  "themeMode",
+  "system",
+  storage,
+  { getOnInit: true }, // Does not use default value, instead gets value from storage on init.
+);
