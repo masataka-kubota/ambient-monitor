d8d23edb19a2d21454ddb9092ce8eb0ea1131fe2
packerman1212 <mezase226334@gmail.com>
2025-12-12 10:48:35 +0900

    feat(mobile): add HeaderNavigation and HeaderBackButton components and add hasHeader prop to KeyboardAvoidingScrollableView component

---
+++ packages/mobile/components/navigation/HeaderBackButton.tsx
@@ -0,0 +1,63 @@
+import { Entypo } from "@expo/vector-icons";
+import { router, useRouter } from "expo-router";
+import React, { memo, useCallback } from "react";
+import { Pressable, StyleSheet } from "react-native";
+
+import { useResolvedTheme } from "@/hooks/common";
+
+const HeaderBackButton = () => {
+  const { canGoBack } = useRouter();
+  const { currentThemeColors } = useResolvedTheme();
+
+  const hasHistory = canGoBack();
+
+  const handlePress = useCallback(() => {
+    if (hasHistory) {
+      router.back();
+    } else {
+      router.replace("/");
+    }
+  }, [hasHistory]);
+
+  return (
+    <Pressable
+      onPress={handlePress}
+      style={[
+        styles.button,
+        {
+          backgroundColor: currentThemeColors.mainColor,
+          shadowColor: currentThemeColors.shadow,
+        },
+      ]}
+    >
+      <Entypo
+        name={hasHistory ? "arrow-left" : "cross"}
+        size={24}
+        color={currentThemeColors.mainBackground}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    position: "absolute",
+    top: 5,
+    left: 10,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex: 1,
+  },
+});
+
+export default memo(HeaderBackButton);
--- packages/mobile/components/navigation/index.tsx
+++ packages/mobile/components/navigation/index.tsx
@@ -1,1 +1,4 @@
+export { default as HeaderBackButton } from "./HeaderBackButton";
+export { default as HeaderNavigation } from "./HeaderNavigation";
 export { default as TabBar } from "./TabBar";
+export { default as TabBarItem } from "./TabBarItem";
