e192ced1127a87b415addf3c9b2d415057d9f820
Masataka <mezase226334@gmail.com>
2026-03-22 07:05:32 +0900

    chore(mobile): update Prettier and ESLint configuration

--- packages/mobile/eslint.config.js
+++ packages/mobile/eslint.config.js
@@ -1,28 +1,46 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig, globalIgnores } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
-  globalIgnores([
-    "dist/*",
-    "/.expo",
-    "node_modules",
-    "expo-env.d.ts",
-    "dist/*",
-  ]),
   expoConfig,
   eslintPluginPrettierRecommended,
 
-  // eslint-plugin-import
+  // ignores
+  {
+    ignores: [
+      ,
+      'dist/*',
+      'node_modules/*',
+      'ios/*',
+      'android/*',
+      '/.expo/*',
+      'expo-env.d.ts',
+    ],
+  },
+
+  // Prettier rules
   {
     rules: {
-      "import/order": [
-        "error",
+      'prettier/prettier': [
+        'error',
         {
-          groups: ["builtin", "external", "internal"],
-          alphabetize: { order: "asc", caseInsensitive: true },
-          "newlines-between": "always",
+          singleQuote: true,
+        },
+      ],
+    },
+  },
+
+  // eslint-plugin-import rules
+  {
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
           warnOnUnassignedImports: true,
         },
       ],
