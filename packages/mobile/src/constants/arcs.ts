8f1671142385fcc0275059a8c46fbeba863c1420
Masataka <mezase226334@gmail.com>
2026-08-14 06:50:06 +0900

    test(mobile): add describeArc coverage and JSDoc for arc helpers

--- packages/mobile/src/constants/arcs.ts
+++ packages/mobile/src/constants/arcs.ts
@@ -1,2 +1,9 @@
+/**
+ * The start angle in degrees used for arc rendering.
+ */
 export const START_ANGLE = -135;
+
+/**
+ * The end angle in degrees used for arc rendering.
+ */
 export const END_ANGLE = 135;
